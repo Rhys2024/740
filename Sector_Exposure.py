7cbab33440c4c7da3b58204677df83bcde06d8a3
Rhys2024 <rhyslogan1@gmail.com>
2022-06-03 13:35:16 -0500

    update

--- Sector_Exposure.py
+++ Sector_Exposure.py
@@ -8,11 +8,12 @@ import os
 from datetime import datetime, timedelta, date
 import json
 from dateutil.relativedelta import relativedelta as delt
+import Useful_Functions as useful
 
 
 class Rate_Exposures(object):
     
-    def __init__(self, df, look_back, forward, compare_against = ['Real Yield'], benchmark = "SP-500"):
+    def __init__(self, df, look_back, forward, compare_against = ['Real Yield'], benchmark = None):
         
         self.look_back = look_back
         self.forward = forward
@@ -20,106 +21,100 @@ class Rate_Exposures(object):
         self.benchmark = benchmark
         self.compare_against = compare_against
         
-        # FIX THISSSSSSSSSSSSSSSSSSS
-        #self.real_yield = self.df[self.compare_against]
-        
-        '''
-        sector_names = ['Energy', 'Materials', 'Industrials', 'Consumer Discretionary', 
-                        'Consumer Staples', 'Health Care',
-                        'Financials', 'Information Technology', 
-                        'Telecommunication Services', 'Utilities', 'Real Estate']
-        '''
-        
-        #self.scores = self.get_scores()
-        
-        #sector_names = [i for i in df.columns if i != compare_against and i != self.benchmark]
-        
-        '''
-        gradient = pd.Series([(grad-self.real_yield[num])/15 for num, grad in enumerate(self.real_yield[15:])], index = [i for i in self.df.index[15:]])
-        gradient_means = gradient.rolling(self.look_back, center=False).mean()
-        gradient_std = gradient.rolling(self.look_back, center=False).std()
-
-        real_yield_means = self.real_yield.rolling(self.look_back, center=False).mean()
-        real_yield_std = self.real_yield.rolling(self.look_back, center=False).std()
-
-        self.standardized_g = pd.Series((gradient[self.look_back:] - gradient_means[self.look_back:])/(gradient_std[self.look_back:]))
-        self.standardized_df = pd.Series((self.real_yield[self.look_back:] - real_yield_means[self.look_back:])/(real_yield_std[self.look_back:]))
-        
-        self.raw_scores = self.standardized_g + self.standardized_df
-        
-        self.scores = self.raw_scores.copy()
-        
-        for num, s in enumerate(self.raw_scores):
-            
-            if round(s, 0) > 3.0 and s < 5.0:
-                self.scores[num] = 4.0
-            elif s > 5.0:
-                self.scores[num] = 5.0
-            elif round(s, 0) < -3.0 and s > -4.5:
-                self.scores[num] = -4.0
-            elif s < -4.5:
-                self.scores[num] = -5.0
-            else:
-                self.scores[num] = round(s, 0)
-        '''
-        
         self.scores = {}
+        self.monthly_scores = {}
         
         for rate in compare_against:
             
             self.scores[rate] = self.get_scores(self.look_back, rate)
-
-        self.sectors = [i for i in df.columns if i != compare_against and i != self.benchmark]
+            self.monthly_scores[rate] = round(self.scores[rate].groupby(pd.PeriodIndex(self.scores[rate].index, freq="M")).mean(), 0)
         
+        
+        self.sectors = [i for i in df.columns if i not in compare_against and i != self.benchmark]
         self.bucket_scores = { i : [] for i in self.sectors}
         
-        ### Fix Correlations and Delete Formatted DF ###
-        self.df_formatted = self.df.copy()
-        self.df_formatted['Date'] = [d for d in self.df.index]
-        self.df_formatted.index = [i for i in range(len(self.df_formatted['Date']))]
-        
-        temp = self.get_total_returns()
-        
+        self.forward_returns_daily = self.get_total_returns()
         for rate in self.compare_against:
-            temp[f"{rate}_scores"] = self.scores[rate]
+            self.forward_returns_daily[f"{rate}_scores"] = self.scores[rate]
+        self.forward_returns_monthly = self.get_monthly_data(self.forward_returns_daily)
+        for rate in self.compare_against:
+            self.forward_returns_monthly[f"{rate}_scores"] = self.monthly_scores[rate]
         
-        self.total_returns = {}
-        self.mean_returns = pd.DataFrame()
         
-        self.ry_total_returns = {}
-        self.ry_mean_returns = pd.DataFrame()
-        self.yc_total_returns = {}
-        self.yc_mean_returns = pd.DataFrame()
-        
-        removes = [f"{self.compare_against[0]}_scores", f"{self.compare_against[1]}_scores"]
-        
-        for score in range(-5,6):
+        ### Signals
+        self.combined_signals = pd.Series([(r,c) for r,c in zip(self.scores['Real Yield'].dropna(), self.scores['Yield Curve'].dropna())], 
+                 index = self.scores['Yield Curve'].dropna().index)
+        self.combined_signal_counts = self.combined_signals.value_counts().to_dict()
             
-            # [f"{self.compare_against[0]}_scores"]
-            self.ry_total_returns[score] = temp.loc[temp[f"{self.compare_against[0]}_scores"] == score].drop(columns = removes).drop(columns = self.compare_against)
-            self.ry_mean_returns[score] = self.ry_total_returns[score].mean()
-            
-            self.yc_total_returns[score] = temp.loc[temp[f"{self.compare_against[1]}_scores"] == score].drop(columns = removes).drop(columns = self.compare_against)
-            self.yc_mean_returns[score] = self.yc_total_returns[score].mean()
-            
-            for score_2 in range(-5,6):
-                
-                self.total_returns[(score, score_2)] = temp.loc[(temp[f"{self.compare_against[1]}_scores"] == score_2) & (temp[f"{self.compare_against[0]}_scores"] == score)].drop(columns = [f"{self.compare_against[0]}_scores", f"{self.compare_against[1]}_scores"]).drop(columns = self.compare_against)
-                self.mean_returns[(score, score_2)] = self.total_returns[(score, score_2)].mean()
         
-        # [(score, score_2)]
+        self.removes = [f"{self.compare_against[0]}_scores", f"{self.compare_against[1]}_scores"]
+        
+        ### DAILY DATA ###
+        self.total_returns = self.get_return_data(self.forward_returns_daily)
+        self.mean_returns = pd.DataFrame({s : self.total_returns[s].mean() for s in self.total_returns})
+        
+        self.ry_total_returns = self.get_return_data(self.forward_returns_daily, "ry")
+        self.ry_mean_returns = pd.DataFrame({s : self.ry_total_returns[s].mean() for s in self.ry_total_returns})
+        self.yc_total_returns = self.get_return_data(self.forward_returns_daily, "yc")
+        self.yc_mean_returns = pd.DataFrame({s : self.yc_total_returns[s].mean() for s in self.yc_total_returns})
+        
         self.mean_returns = self.mean_returns.dropna(axis = 1).T
         self.mean_returns.index = pd.MultiIndex.from_tuples(list(self.mean_returns.index))
         self.ry_mean_returns = self.ry_mean_returns.T
         self.yc_mean_returns = self.yc_mean_returns.T
+        ###
         
+        ### MONTHLY DATA ###
+        self.total_returns_monthly = self.get_return_data(self.forward_returns_monthly)
+        self.mean_returns_monthly = pd.DataFrame({s : self.total_returns_monthly[s].mean() for s in self.total_returns_monthly})
+        
+        self.ry_total_returns_monthly = self.get_return_data(self.forward_returns_monthly, "ry")
+        self.ry_mean_returns_monthly = pd.DataFrame({s : self.ry_total_returns_monthly[s].mean() for s in self.ry_total_returns_monthly})
+        self.yc_total_returns_monthly = self.get_return_data(self.forward_returns_monthly, "yc")
+        self.yc_mean_returns_monthly = pd.DataFrame({s : self.yc_total_returns_monthly[s].mean() for s in self.yc_total_returns_monthly})
+        
+        self.mean_returns_monthly = self.mean_returns_monthly.dropna(axis = 1).T
+        self.mean_returns_monthly.index = pd.MultiIndex.from_tuples(list(self.mean_returns_monthly.index))
+        self.ry_mean_returns_monthly = self.ry_mean_returns_monthly.T
+        self.yc_mean_returns_monthly = self.yc_mean_returns_monthly.T
+        ###
+        
+        
+    
+    
+    def get_return_data(self, returns_data, rate = "both"):
+        
+        temp = {}
+        
+        for score in range(-6,7):
+            
+            if "real" in rate.lower() or "ry" in rate.lower():
+                consider = f"{self.compare_against[0]}_scores"
+            elif "curve" in rate.lower() or "yc" in rate.lower():
+                consider = f"{self.compare_against[1]}_scores"
+            if rate.lower() != "both":
+                # .drop(columns = self.compare_against)
+                temp[score] = returns_data.loc[returns_data[consider] == score].drop(columns = self.removes)
+        
+            if rate.lower() == "both":
+                
+                for score_2 in range(-6,7):
+                    # .drop(columns = self.compare_against)
+                    data = returns_data.loc[(returns_data[f"{self.compare_against[1]}_scores"] == score_2) & 
+                                            (returns_data[f"{self.compare_against[0]}_scores"] == score)].drop(columns = [f"{self.compare_against[0]}_scores", 
+                                                                                                                          f"{self.compare_against[1]}_scores"])
+                    
+                    if len(data) > 0:
+                        temp[(score, score_2)] = data
+        
+        return temp
+    
         
     def get_scores(self, lookback, target):
     
         target_rate = self.df[target]
 
-        #print(target_rate)
-        gradient = pd.Series([(grad-target_rate[num])/15 for num, grad in enumerate(target_rate[15:])], index = [i for i in self.df.index[15:]])
+        gradient = pd.Series([(grad-target_rate[num])/5 for num, grad in enumerate(target_rate[5:])], index = [i for i in self.df.index[5:]])
         gradient_means = gradient.rolling(lookback, center=False).mean()
         gradient_std = gradient.rolling(lookback, center=False).std()
 
@@ -132,104 +127,158 @@ class Rate_Exposures(object):
         raw_scores = standardized_g + standardized_df
         
         scores = round(raw_scores, 0)
-        
-        #self.scores = self.raw_scores.copy()
 
-        return scores
+        return scores.dropna()
+    
+    def get_monthly_data(self, data):
+        
+        return data.groupby(pd.PeriodIndex(data.index, freq="M"))[[i for i in data.columns]].mean()
                 
 
     def get_total_returns(self):
         
-        #take_out = self.compare_against.copy()
-        #take_out.append(self.benchmark)
-        
-        #frame = self.df[[list(self.sectors)]]
-        
-        ok = {}
+        rets = {}
         
         for num, date in enumerate(self.df.index):
             
             if (num+self.forward < len(self.df)):
                 
-                ok[date] = (((self.df[list(self.sectors)].iloc[num+self.forward] / self.df[list(self.sectors)].iloc[num]) - 1) - 
+                if self.benchmark:
+                    
+                    rets[date] = (((self.df[list(self.sectors)].iloc[num+self.forward] / self.df[list(self.sectors)].iloc[num]) - 1) - 
                             ((self.df[self.benchmark].iloc[num+self.forward] / self.df[self.benchmark].iloc[num]) - 1))
+                
+                else:
+                    
+                    rets[date] = (self.df[list(self.sectors)].iloc[num+self.forward] / self.df[list(self.sectors)].iloc[num]) - 1
+                    
         
-        temp = ok.copy()
+        temp = rets.copy()
         for t in temp:
             if len(temp[t]) == 0:
-                ok.pop(t)
-                
-        ok = pd.DataFrame(ok).T
-        ok.columns = self.sectors
+                rets.pop(t)
         
-        return ok
+        rets = pd.DataFrame(rets).T
+        rets.columns = self.sectors
+        
+        return rets
 
     
-    def get_correlations(self, window = None):
+    def get_correlations(self, window = None, rate = 'both', full = False):
         
-        if not window:
-            print('Please enter a value for window size')
+        '''
         
+        Full being False inplies that the data is showing correlation Means
         
-        self.correlations = { i : { sec : deque() for sec in self.sectors} for i in range(-4,5)}
-        self.correlations = { i : { sec : deque() for sec in self.sectors} for i in range(-4,5)}
+        '''
+        
+        assert window, 'Please enter a value for window size'
+        assert isinstance(window, int), "window must be an int"
+        assert window > 0, "please enter a positive integer for window"
+        assert isinstance(full, bool), "full must be a boolean"
+        
+        correlations = {}
+        
+        drops = [col for col in self.df.columns if col == self.benchmark or col in self.compare_against]
+        
+        consider_df = self.df.pct_change()
 
-        for i in self.scores:
-            for date in self.scores[i]:
+        for num, date in enumerate(self.df.index):
+            
+            if num > 0 and (num+window < len(self.df)):
                 
-                df_index = self.df_formatted.index[self.df_formatted['Date'] == date][0]
-
+                # (self.df[num:num+window].corr()[self.benchmark])
+                correlations[date] = {}
+                
                 for sec in self.sectors:
-
-                    if window > 0:
-                        if (df_index+self.forward < len(self.df)) and (self.df[sec].iloc[df_index] != 0.0):
-                        
-                            self.correlations[i][sec].append(self.df[df_index:df_index+window].corr()[sec]['Real Yield'])
-                    else:
-                        if (df_index+self.forward < len(self.df)) and (self.df[sec].iloc[df_index] != 0.0):
-                        
-                            self.correlations[i][sec].append(self.df[df_index+window:df_index].corr()[sec]['Real Yield'])
-                        
-        self.correlation_means = { i : { sec : np.mean(self.correlations[i][sec]) for sec in self.sectors} for i in range(-4,5)}
+                    correlations[date][sec] = useful.beta_asset_to_index(consider_df[[sec, self.benchmark]][num:num+window].values)
+                    
+        # .drop(columns = [self.benchmark])
+        correlations = pd.DataFrame(correlations).T
         
-        self.df_correlation_means = pd.DataFrame(self.correlation_means).T
+        
+        for num, score in enumerate(self.scores):
+            
+            correlations[f"{self.compare_against[num]}_scores"] = self.scores[score]
+        
+        correlations = self.get_return_data(correlations.dropna(), rate)
 
-        return self.df_correlation_means
+        if not full:
+            return pd.DataFrame({score : correlations[score].mean() for score in correlations}).T
+        else:
+            return correlations
     
     
-    def show_return_distribution(self, sec = None, score = None, rate = "Both"):
+    def show_return_distribution(self, monthly = True, sec = None, score = None, rate = "Both"):
         
         assert sec, score
         assert type(sec) == str and (type(score) == int or type(score) == tuple)
         
-        
         plt.figure(figsize=(12,8))
+        
         if type(score) == tuple:
-            self.total_returns[score][sec].hist()
+            if monthly:
+                data = self.total_returns_monthly
+            else:
+                data = self.total_returns
+            data[score][sec].hist()
             plt.title(f"{(score[0], score[1])} Signals for {sec} Sector", size = 20)
             plt.xlabel(f"{self.forward} Day Forward Returns", size = 14)
-            plt.axvline(np.mean(self.total_returns[score][sec]), c = 'r')
+            plt.axvline(np.mean(data[score][sec]), c = 'r')
             
         else:
+            
             if "real" in rate:
-                self.ry_total_returns[score][sec].hist()
+                if monthly:
+                    data = self.ry_total_returns_monthly
+                else:
+                    data = self.ry_total_returns
+                data[score][sec].hist()
                 plt.title(f"{score} Signals for {sec} Sector", size = 20)
                 plt.xlabel(f"{self.forward} Day Forward Returns", size = 14)
-                plt.axvline(np.mean(self.ry_total_returns[score][sec]), c = 'r')
+                plt.axvline(np.mean(data[score][sec]), c = 'r')
                 
             elif "curve" in rate:
-                self.yc_total_returns[score][sec].hist()
+                if monthly:
+                    data = self.yc_total_returns_monthly
+                else:
+                    data = self.yc_total_returns
+                data[score][sec].hist()
                 plt.title(f"{score} Signals for {sec} Sector", size = 20)
                 plt.xlabel(f"{self.forward} Day Forward Returns", size = 14)
-                plt.axvline(np.mean(self.yc_total_returns[score][sec]), c = 'r')
+                plt.axvline(np.mean(data[score][sec]), c = 'r')
         
         plt.tight_layout()
         plt.show()
-        #else:
-            #plt.figure(figsize=(12,8))
-            #self.total_returns[score][sec].hist()
-            #plt.title(f"{score} Signals for {sec} Sector", size = 20)
-            #plt.xlabel(f"{self.forward} Day Forward Returns", size = 14)
-            #plt.axvline(np.mean(self.total_returns[score][sec]), c = 'r')
-            #plt.tight_layout()
-            #plt.show()
+        
+        
+
+    def signal_dates_and_returns(self, monthly = True, sector = None, scores = None, rate = "both"):
+        
+        assert self.total_returns, self.total_returns_monthly
+        assert self.ry_total_returns, self.ry_total_returns_monthly
+        assert self.yc_total_returns, self.yc_total_returns_monthly
+        
+        assert sector in self.sectors
+        assert scores
+        
+        if rate.lower() == "both" or isinstance(scores, tuple):
+            
+            if monthly:
+                return self.total_returns_monthly[scores][sector].sort_values()
+            else:
+                return self.total_returns[scores][sector].sort_values()
+        
+        elif ("real" in rate.lower() or "ry" in rate.lower()) and isinstance(scores, int):
+            
+            if monthly:
+                return self.ry_total_returns_monthly[scores][sector].sort_values()
+            else:
+                return self.ry_total_returns[scores][sector].sort_values()
+        
+        elif ("curve" in rate.lower() or "yc" in rate.lower()) and isinstance(scores, int):
+            
+            if monthly:
+                return self.yc_total_returns_monthly[scores][sector].sort_values()
+            else:
+                return self.yc_total_returns[scores][sector].sort_values()
--- Useful_Functions.py
+++ Useful_Functions.py
@@ -2,8 +2,9 @@ import pandas as pd
 import yfinance as yf
 import fredapi as fa
 from datetime import date, timedelta
-from Sector_Exposure import CompareSectors
+#from Sector_Exposure import Rate_Exposures
 import numpy as np
+
 
 
 def beta(data_2_columns):
@@ -11,4 +12,116 @@ def beta(data_2_columns):
     log_returns = np.log(data_2_columns/data_2_columns.shift())
     
     return (log_returns.cov() / log_returns.var())[log_returns.columns[0]].iloc[1]
+
+
+def beta_asset_to_index(asset_to_index):
+    
+    assert isinstance(asset_to_index, np.ndarray), "Make sure input data is a Numpy Array !!"
+    assert asset_to_index.shape[1] == 2, "Make sure input data has 2 columns !!"
+    
+    asset_to_index = np.vstack((asset_to_index[:,0], asset_to_index[:,1]))
+    
+    #log_returns = np.log(asset_to_index/asset_to_index.shift()).values
+    
+    return np.cov(asset_to_index)[0][1] / np.var(asset_to_index)
+
+
+def current_rates(window = "m", descrip = True):
+    
+    assert window == "m" or window == "d" or "mon" in window.lower() or window.lower() == "day", "window should be 'd' or 'm'"
+    
+    fred = fa.Fred('4fb0ce271d0f66f4b5b3904b4aaf1dd0')
+    
+    
+    real_rate = fred.get_series('DFII10', observation_start = date.today() + timedelta(days= -600) , end = date.today())
+    yield_curve = fred.get_series('T10Y2Y', observation_start = date.today() + timedelta(days= -600) , end = date.today())
+    
+    scored_ry = get_scores(252, real_rate.dropna())
+    scored_yc = get_scores(252, yield_curve.dropna())
+    
+    if descrip:
+        print("past month Real Yield, Yield Curve:\n")
+        
+    if window == "m":
+        past_month_ry = round(scored_ry[-30:].mean(), 0)
+        past_month_yc = round(scored_yc[-30:].mean(), 0)
+        return (past_month_ry, past_month_yc)
+    elif window == "d":
+        return (scored_ry.iloc[-1], scored_yc.iloc[-1])
+
+def get_scores(lookback, target):
+    
+    target_rate = target
+    
+    gradient = pd.Series([(grad-target_rate[num])/15 for num, grad in enumerate(target_rate[15:])], index = [i for i in target.index[15:]]).dropna()
+    
+    gradient_means = gradient.rolling(lookback, center=False).mean()
+    gradient_std = gradient.rolling(lookback, center=False).std()
+    
+    target_rate_means = target_rate.rolling(lookback, center=False).mean()
+    target_rate_std = target_rate.rolling(lookback, center=False).std()
+    standardized_g = pd.Series((gradient[lookback:] - gradient_means[lookback:])/(gradient_std[lookback:]))
+    standardized_df = pd.Series((target_rate[lookback:] - target_rate_means[lookback:])/(target_rate_std[lookback:]))
+    
+    raw_scores = standardized_g + standardized_df
+    
+    scores = round(raw_scores, 0)
+    
+    return scores
+
+
+# compare_group = None
+# assert compare_group
+def update_data():
+    
+    fred = fa.Fred('4fb0ce271d0f66f4b5b3904b4aaf1dd0')
+    
+    # "ten", "two",
+    sector_names = ['Materials', 'Industrials', 'Consumer Discretionary', 
+                        'Consumer Staples', 'Health Care',
+                        'Financials', 'Information Technology', 
+                        'Telecommunication Services', 'Utilities', 'Real Estate', 'Energy', 'Semiconductors', "Aerospace",
+                        "SP-500", "Real Yield", "Yield Curve"]
+    
+    # "ten", "two",
+    factor_names = ['Value', 'Quality', 'Size', 'Default', "Real Yield", "Yield Curve"]
+    
+    #if 'sector' in compare_group.lower():
+        
+    sec = yf.download([f"^SP500-{i}" for i in range(15,65,5)], start = '2010-01-04', progress=False)["Close"]
+    sec['Energy'] = yf.download(["^GSPE"], start = '2010-01-04', progress=False)["Close"]
+    sec['Semiconductors'] = yf.download(["SOXX"], start = '2010-01-04', progress=False)["Close"]
+    sec['Aerospace'] = yf.download(["ITA"], start = '2010-01-04', progress=False)["Close"]
+    sec['SP-500'] = yf.download("^GSPC", start = '2010-01-04', progress=False)["Close"]
+    sec['Real Yield'] = fred.get_series('DFII10', observation_start = '2010-01-04', end = date.today())
+    sec['Yield Curve'] = fred.get_series('T10Y2Y', observation_start = '2010-01-04', end = date.today())
+        
+    #if 'factor' in compare_group.lower():
+        
+    fact = pd.DataFrame()
+    fact['Value'] = yf.download(["VLUE"], start = '2010-01-04', progress=False)["Close"]
+    fact['Quality'] = yf.download(["QUAL"], start = '2010-01-04', progress=False)["Close"]
+    fact['Size'] = yf.download(["SIZE"], start = '2010-01-04', progress=False)["Close"]
+    fact['Default'] = yf.download(["FIBR"], start = '2010-01-04', progress=False)["Close"]
+    fact['Real Yield'] = sec['Real Yield']
+    fact['Yield Curve'] = sec['Yield Curve']
+    
+    sec.columns = sector_names
+    fact.columns = factor_names
+    
+    #sectors = pd.DataFrame(sec.values, columns = sector_names, index = sec.index)
+    
+    #sec['ten'] = fred.get_series('DGS10', observation_start = '2010-01-04', end = date.today())
+    #sec['two'] = fred.get_series('DGS2', observation_start = '2010-01-04', end = date.today())
+    
+    sec.dropna().to_csv("sectors_and_rates.csv")
+    fact.dropna().to_csv("factors_and_rates.csv")
+    
+    #if 'sector' in compare_group.lower():
+        #sec.columns = sector_names
+        #return sec.dropna()
+    
+    #if 'factor' in compare_group.lower():
+        #sec.columns = factor_names
+        #return sec.dropna()
 
